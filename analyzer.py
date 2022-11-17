c2dfae50724550cf852551eec06f280c113688db
cheerfulleg <yolginivan@gmail.com>
2022-11-17 15:53:59 +0200

    added logging

--- analyzer.py
+++ analyzer.py
@@ -1,9 +1,16 @@
 import glob
+import logging
 import os
 import shutil
 
 import radon.raw
 from git.repo.base import Repo
+
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+logger.addHandler(ch)
 
 
 class Analyzer:
@@ -15,6 +22,7 @@ class Analyzer:
         analytics = radon.raw.analyze(source)._asdict()
         analytics['classes_qnt'] = source.count('class ')
         analytics['functions_qnt'] = source.count('def ')
+        logger.info(analytics)
         return analytics
 
     def _get_source_code(self, repo: str) -> str:
@@ -25,22 +33,30 @@ class Analyzer:
                     source += f.read() + '\n'
         return source
 
+    @staticmethod
+    def _download_project(repo: str):
+        url = f'https://github.com/{repo}'
+        Repo.clone_from(url, repo)
+
+    def _delete_directory(self, repo: str):
+        delete_dir = os.path.join(self.BASE_DIR, repo.split('/')[0])
+        shutil.rmtree(delete_dir)
+        logger.info(f'{repo} deleted')
+
     def get_metrics(self, repos: list[str]) -> list[dict]:
         result = []
         for repo in repos:
             try:
-                url = f'https://github.com/{repo}'
-                Repo.clone_from(url, repo)
+                self._download_project(repo)
             except Exception as e:
-                print(e)
+                logger.error(e)
                 break
+
             try:
                 source = self._get_source_code(repo)
                 result.append(self._analyze(source))
             except Exception as e:
-                print(e)
+                logger.error(e)
             finally:
-                # delete downloaded project
-                delete_dir = os.path.join(self.BASE_DIR, repo.split('/')[0])
-                shutil.rmtree(delete_dir)
+                self._delete_directory(repo)
         return result
