19479053412a3ffef28730a17ea37897514c87c1
cheerfulleg <yolginivan@gmail.com>
2022-11-17 15:53:38 +0200

    added argparser

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 
 from analyzer import Analyzer
 from csv_writer import write_to_csv
@@ -6,29 +6,24 @@ from scrapper import Scrapper
 
 COLUMNS = ['loc', 'lloc', 'sloc', 'comments', 'multi', 'single_comments', 'blank', 'classes_qnt', 'functions_qnt']
 
-"""   * **loc**: The number of lines of code (total)
-        * **lloc**: The number of logical lines of code
-        * **sloc**: The number of source lines of code (not necessarily
-            corresponding to the LLOC)
-        * **comments**: The number of Python comment lines
-        * **multi**: The number of lines which represent multi-line strings
-        * **single_comments**: The number of lines which are just comments with
-            no code
-        * **blank**: The number of blank lines (or whitespace-only ones)
-"""
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--tag', help='GitHub Topics Tag')
+    parser.add_argument('--file', default='result.csv', help='Results filename')
+    parser.add_argument('--start', default=1, help='Start page')
+    parser.add_argument('--end', default=2, help='End page')
+    return parser.parse_args()
 
 
 def run():
-    tag = sys.argv[1]
-    filename = sys.argv[2]
-    range_to = int(sys.argv[3])
-    range_from = int(sys.argv[4])
+    args = get_args()
 
     scrapper = Scrapper()
     analyzer = Analyzer()
-    repos = scrapper.get_list_of_repos(tag=tag, range_from=range_from, range_to=range_to)
+    repos = scrapper.get_list_of_repos(tag=args.tag, range_from=int(args.start), range_to=int(args.end))
     metrics = analyzer.get_metrics(repos)
-    write_to_csv(metrics, COLUMNS, filename)
+    write_to_csv(metrics, COLUMNS, args.filename)
 
 
 if __name__ == '__main__':
